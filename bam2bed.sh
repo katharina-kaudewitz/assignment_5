a0fd798e30979450bbc0b0840c9edfa77d1adec5
katharina-kaudewitz <katharina.kaudewitz@gmx.de>
2025-04-23 11:50:56 +0200

    changed line count

--- bam2bed.sh
+++ bam2bed.sh
@@ -16,14 +16,13 @@ source $(dirname $(dirname $(which mamba)))/etc/profile.d/conda.sh
 conda create -y -n bam2bed bedtools
 conda activate bam2bed
 
-#convert bam to bed
+#convert bam to bed + check output of bed file
 bam_file_name=$(basename "$input_file" .bam)
-echo "BAM file name: $bam_file_name"
+echo "Bam file name: $bam_file_name"
 
 bed_file="$output_dir/${bam_file_name}.bed"
 bedtools bamtobed -i "$input_file" > "$bed_file"
-
-echo "BED file name: $bed_file"
+echo "Bed file stored  at: $bed_file"
 
 #create bed file with only chromosome 1
 file_chr1="$output_dir/${bam_file_name}_chr1.bed"
@@ -32,8 +31,8 @@ file_chr1="$output_dir/${bam_file_name}_chr1.bed"
 grep -P "^Chr1\t" "$bed_file" > "$file_chr1"
 
 #count lines
-line_count=$(wc -l < "$file_chr1")
-echo "$line_count"  > "$output_dir/bam2bed_number_of_rows.txt"
+wc -l  "$file_chr1" > "$output_dir/bam2bed_number_of_rows.txt"
+echo  "Textfile stored at: $output_dir/bam2bed_number_of_rows.txt"
 
 #my name
 echo "This script was created by Katharina Kaudewitz"
